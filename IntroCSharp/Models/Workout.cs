c18319844646122f19245e860cbda06cbbf9f0b8
Paul de Mast <pjf.demast@avans.nl>
2022-02-01 15:20:48 +0100

    Add class Workout

---
+++ IntroCSharp/Models/Workout.cs
@@ -0,0 +1,24 @@
+﻿namespace IntroCSharp.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        // nullable value type:
+        public bool? IsOutside { get; set; }
+
+        // get-only calculated property:
+        public DateTime EndTime { get { return StartTime + Duration; } }
+
+        public Workout(string title, DateTime startTime, TimeSpan duration, decimal price = 0M) {
+            Title = title;
+            StartTime = startTime;
+            Duration = duration;
+            Price = price;
+        }
+    }
+}
